b1b157353667c179f9e6f3f2fbb79851264eddd5
hostnomics <hostnomics@gmail.com>
2023-07-03 23:59:46 -0400

    Added FooterColumn and ColumnProps to Footer.tsx component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,27 @@
+import { footerLinks } from '@/constants/constants'
 import Image from 'next/image'
+import Link from 'next/link';
 import React from 'react'
 
-//Built out (26:27): https://youtu.be/986hztrfaSQ?t=1587
+type ColumnProps = {
+    title: string;
+    links: Array<string>;
+}
 
+//Added FooterColumn Utility/Helper Compoennt 28:40 with immediate return () instead of {}
+const FooterColumn = ({title, links}: ColumnProps) => (
+    <div className="footer_column">
+        <h4 className="font-semibold">{title}</h4>
+        <ul className="flex flex-col gap-2 font-normal">
+            {links.map((link) => <Link href="/" key={link}>
+                {link}
+            </Link>)}
+        </ul>
+    </div>
+)
+
+
+//Built out (26:27): https://youtu.be/986hztrfaSQ?t=1587
 const Footer = () => {
   return (
     <footer className="flexStart footer">
@@ -18,7 +37,13 @@ const Footer = () => {
                 <p className="text-start text-sm font-normal mt-5 max-w-xs">
                     Flexibble is the world's leading community for patients to send and fill their prescriptions on the blockchain.
                 </p>
-            </div>        
+            </div>  
+            <div className="flex flex-wrap gap-12">
+                <FooterColumn 
+                    title={footerLinks[0].title}
+                    links={footerLinks[0].links}
+                />
+            </div>      
         </div>
     </footer>
   )
